ac36da122da58ffac33a4b5b6360dd40d2e23da5
Thomas Garcia <thomasjgarcia1@gmail.com>
2016-01-17 04:26:05 -0800

    fixed up dynamic tables

--- public/javascripts/fillTable.js
+++ public/javascripts/fillTable.js
@@ -2,45 +2,50 @@ $(function(){
     var table = document.getElementById('myTable');
     setInterval(function() {
         $.ajax({
-            url: 'http://131.179.50.179:3000/data/',
+            url: '/data',
             success: function(data) {
-                console.log(data);
-                var str = data.string;
-                var cmd = "";
-                var newRow = table.insertRow(0);
-                var newCell  = newRow.insertCell(0);
-                switch(str)
-                {
-                    case "LO":
-                       cmd = "Lights on";
-                       break;
-                   case "LC":
-                       cmd = "Lights off";
-                       break;
-                   case "LOO":
-                       cmd = "Raise Brightness";
-                       break;
-                   case "LCC":
-                       cmd = "Lower Brightness";
-                       break;
-                   case "OO":
-                       cmd = "Raise volume";
-                       break;
-                   case "CC":
-                       cmd = "Lower volume";
-                       break;
-                   case "OC":
-                       cmd = "Lock door";
-                       break;
-                   case "CO":
-                       cmd = "Open door";
-                       break;
-                   default:
-                       cmd="Command not recognized";
-                       break;
+                len = table.rows.length;
+                for(i = 0; i < len; i++) {
+                    table.deleteRow(0);
                 }
-                var newText  = document.createTextNode(cmd);
-                newCell.appendChild(newText);
+                for(i = 0; i < data.length; i++) {
+                    var str = data[i].str;
+                    var cmd = "";
+                    var newRow = table.insertRow();
+                    var newCell  = newRow.insertCell();
+                    switch(str)
+                    {
+                        case "LO":
+                           cmd = "Lights on";
+                           break;
+                       case "LC":
+                           cmd = "Lights off";
+                           break;
+                       case "LOO":
+                           cmd = "Raise Brightness";
+                           break;
+                       case "LCC":
+                           cmd = "Lower Brightness";
+                           break;
+                       case "OO":
+                           cmd = "Raise volume";
+                           break;
+                       case "CC":
+                           cmd = "Lower volume";
+                           break;
+                       case "OC":
+                           cmd = "Lock door";
+                           break;
+                       case "CO":
+                           cmd = "Open door";
+                           break;
+                       default:
+                           cmd="Command not recognized";
+                           break;
+                    }
+                    var newText  = document.createTextNode(cmd);
+                    newCell.appendChild(newText);
+                }
             },
             dataType: "json"});
     },
